b3afc51e387267c58848280a92da9db1af1ba43b
miladtaghizadeh1382 <100273410+miladtaghizadeh1382@users.noreply.github.com>
2022-02-26 23:05:09 +0330

    Add files via upload
    Taklif 1 Jalase 2

---
+++ tamrin1s2/src/tamrin1s2/tamrin1s2.java
@@ -0,0 +1,27 @@
+package tamrin1s2;
+import java.awt.*;
+import java.util.Scanner;
+public class tamrin1s2 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String shekl=sc.nextLine() ;
+        float tul = sc.nextFloat() ;
+        float arz = sc.nextFloat() ;
+        float mohit = tul+tul+arz+arz ;
+        System.out.println("P = " + mohit);
+        switch (shekl) {
+            case "square" :
+                System.out.println("S = " + tul*arz) ;
+                break ;
+            case "rectangle" :
+                System.out.println("S = " + tul*arz);
+                break ;
+        }
+
+
+    }
+
+
+}
+
